6a9f28a69d6e54030038ef32eb59c25ba4174254
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-05 13:24:31 +0200

    realitzacio 31_09

---
+++ 31_09_operadors_relacionals/operadors_realcionals.java
@@ -0,0 +1,24 @@
+edat > 18		/* la teva edat és superior als 18 anys */
+
+germans < 5		/* tens menys de 5 germans */
+
+2 + 5 = 7		/* la suma de 2 més 5 és igual a 7 */
+
+2 + 5 != 7	/* la suma de 2 més 5 és diferent de 7 */
+
+(edat + 1) >= 18	/* l'any que ve seràs major d'edat */
+
+(edat - 1) < 18	/* l'any passat encara no eres major d'edat */
+
+numCompanys <= 35	/* tens com a molt 35 companys a classe */
+
+companys = anys	/* tens tants companys a classe com anys */
+
+potes == 4	/* el Renat té quatre potes */
+
+temperatura > 28	/* estem a més de 28 graus */
+
+ordinadors < alumnes	/* el número d'ordinadors és inferior als alumnes */
+
+alumnes != taules	/* no tenim els mateixos alumnes que taules */
+
