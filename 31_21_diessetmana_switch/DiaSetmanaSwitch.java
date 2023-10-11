c0b3f6a5b45d1f42652b7335c0c2c6a5f8aaaed3
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-11 11:52:43 +0200

    prgtest 31_21

---
+++ 31_21_diessetmana_switch/DiaSetmanaSwitch.java
@@ -0,0 +1,25 @@
+public class DiaSetmanaSwitch {
+	public static void main(String[] args) {
+		int num = Integer.parseInt(args[0]);
+		String dia = "";
+		switch (num) {
+			case 1:	dia = "Dilluns";
+						break;
+			case 2:	dia = "Dimarts";
+						break;
+			case 3:	dia = "Dimecres";
+						break;
+			case 4:	dia = "Dijous";
+						break;
+			case 5:	dia = "Divendres";
+						break;
+			case 6:	dia = "Dissabte";
+						break;
+			case 7:	dia = "Diumenge";
+						break;
+			default:	dia = "Error";
+						break;
+		}
+		System.out.println(dia);
+	}
+}
