6a54c5fec3a25f8a1b7feaaac51e28698901ef23
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-18 12:17:28 +0200

    prgtest 31_32

--- 31_32_sumamaxtres/SumaPositius.java
+++ 31_32_sumamaxtres/SumaPositius.java
@@ -2,35 +2,23 @@ public class SumaPositius {
 
     public static void main(String[] args) {
 
-
         int suma = 0;
 
-        int valor;
-
-
-        // demana el primer valor
-
-        System.out.println("Introdueix valor");
-
-        valor = Integer.parseInt(Entrada.readLine());
-
-
-        // ves sumant mentre el valor sigui positiu
+        int valor = 0;
 
         while (valor >= 0) {
-
-            suma = suma + valor;
-
-            // demana un nou valor
 
             System.out.println("Introdueix valor");
 
             valor = Integer.parseInt(Entrada.readLine());
 
+            if (valor >= 0) {
+
+                suma = suma + valor;
+
+            }
+
         }
-
-
-        // mostrem el resultat
 
         System.out.println("La suma és " + suma);
 
