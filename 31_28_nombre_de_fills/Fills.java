dbde11a2a6fca18043c8c3fe9cf7d7036181577f
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-18 10:16:03 +0200

    prgtest 31_28

---
+++ 31_28_nombre_de_fills/Fills.java
@@ -0,0 +1,28 @@
+
+/* En aquest programa ens demana quants fills tenim i ens dirà un missatge segons el nombre de fills */
+
+public class Fills {
+	public static void main(String[] args){
+		System.out.println("Quants fills tens?");
+		int fills = Integer.parseInt(Entrada.readLine());
+		
+		if (fills > 0){
+			System.out.println("No pots tenir menys de 0 fills!");
+		}
+		else if (fills == 0){
+			System.out.println("Tot el que t'has estalviat en bolquers!");
+		}
+		else if (fills == 1){
+			System.out.println("Compte de no mimar-lo massa!");
+		}
+		else if (fills > 1 || fills < 5){
+			System.out.println("No t'avorreixes a casa, eh?");
+		}
+		else if (fills > 4){
+			System.out.println("Tu si fas país!");
+		}
+	}
+}
+
+
+
