4c9ae70a3ad6118b759a82ce4bd7de0cbccb167d
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-25 10:35:17 +0200

    prgtest 31_49

---
+++ 31_49_de_principi_a_fi_for/EntersIniciFi.java
@@ -0,0 +1,18 @@
+
+/* En aquest programa preguntem un valor d'inici, fi i el salt. I imprimim els nombres que 
+	hi han entre els valors amb el salt */
+	
+public class EntersIniciFi {
+	public static void main(String[] args){
+		System.out.println("Valor inicial?");
+		int numInicial = Integer.parseInt(Entrada.readLine());
+		System.out.println("Valor final?");
+		int numFinal = Integer.parseInt(Entrada.readLine());
+		System.out.println("Salt?");
+		int salt = Integer.parseInt(Entrada.readLine());
+		
+		for (int i=salt; numInicial>numFinal; i=i+salt){
+			System.out.println(i);
+		}
+	}
+}
