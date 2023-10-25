7ba8c67c495645bf75f6e5e5d45c96d0b9522c47
prgtest <2023_pau.pedros@iticbcn.cat>
2023-10-25 10:39:55 +0200

    prgtest 31_49  ∾ prgtest autocommit 31_49 #4

--- 31_49_de_principi_a_fi_for/EntersIniciFi.java
+++ 31_49_de_principi_a_fi_for/EntersIniciFi.java
@@ -11,7 +11,7 @@ public class EntersIniciFi {
 		System.out.println("Salt?");
 		int salt = Integer.parseInt(Entrada.readLine());
 		
-		for(int i=numInicial; i<numFinal; i+=salt){
+		for(int i=numInicial; i<=numFinal; i+=salt){
 			System.out.println(i);
 		}
 	}
