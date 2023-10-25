108a414d5bc8b91878d3db8936a38eaa137f2cb6
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-25 10:16:44 +0200

    prgtest 31_47

---
+++ 31_47_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,11 @@
+
+/* En aquest programa utilitzarem el bulce for per imprimir els deu primers nombres 
+	naturals en ordre decreixent*/
+
+public class DeuPrimersDecreixent{
+	public static void main(String[] args){
+		for (int i=10; i>=1; i--){
+			System.out.println(i);
+		}
+	}
+}
