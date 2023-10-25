83357de573bedc826e2de3fc5e6ce9cba82f4753
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-25 10:22:51 +0200

    prgtest 31_48

---
+++ 31_48_primers_parells_for/PrimersParells.java
@@ -0,0 +1,10 @@
+
+/* En aquest programa imprimirem els 10 primers parells naturals */
+
+public class PrimersParells{
+	public static void main(String[] args){
+		for (int i=2; i<=10; i=i+2){
+			System.out.println(i);
+		}
+	}
+}
