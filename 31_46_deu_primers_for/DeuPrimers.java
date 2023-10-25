c8371cacf4fdd2389ce4abb2ed8f85afa91e67a3
paupedros <2023_pau.pedros@iticbcn.cat>
2023-10-25 10:14:01 +0200

    prgtest 31_46

---
+++ 31_46_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,10 @@
+
+/* En aquest programa utilitzarem el bulce for per imprimir els deu primers nombres naturals */
+
+public class DeuPrimers{
+	public static void main(String[] args){
+		for (int i=1; i<=10; i++){
+			System.out.println(i);
+		}
+	}
+}
