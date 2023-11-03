c4654d32121a3c4d621f399282ae300e8dcf327e
prgtest <2023_pau.pedros@iticbcn.cat>
2023-11-03 10:25:08 +0100

    31_64  ∾ prgtest autocommit 31_64 #2

--- 31_64_manipulant_strings/ManipulaString.java
+++ 31_64_manipulant_strings/ManipulaString.java
@@ -15,12 +15,14 @@ public class ManipulaString {
 
 		    
 
-		    // Mostra la penúltima lletra de la paraula
-		    System.out.println("La penúltima lletra és '" + paraula.charAt(paraula.length()-2) + "'");
+		   
 		}
         // Mostra la darrera lletra de la paraula
 			System.out.println("La darrera lletra és '" + paraula.charAt(paraula.length()-1) + "'");
 
+		 // Mostra la penúltima lletra de la paraula
+		    System.out.println("La penúltima lletra és '" + paraula.charAt(paraula.length()-2) + "'");
+		    
         // Mostra la paraula amb totes les lletres en majúscules
         System.out.println("La paraula en majúscules és " + "\"" + paraula.toUpperCase() + "\"");
 		
