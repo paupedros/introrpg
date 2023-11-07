4ffff25a93df52b48134f93acafab86b29eb9d17
prgtest <2023_pau.pedros@iticbcn.cat>
2023-11-07 11:49:54 +0100

    prgtest 31_69  ∾ prgtest autocommit 31_69 #2

--- 31_69_matricula_italiana/MatriculaValida.java
+++ 31_69_matricula_italiana/MatriculaValida.java
@@ -7,85 +7,83 @@ public class MatriculaValida {
 		System.out.println("Introduïu una matrícula");
 		String matricula = Entrada.readLine();
 		
-		String lletresInicial = matricula.substring(0,2);
-		String nums = matricula.substring(2,5);
-		String lletresFinal = matricula.substring(5,7);
+		if (matricula.length() != 6){
 		
-		boolean boolInici = false;
-		boolean boolNums = false;
-		boolean boolFinal = false;
-		
-		// Validació primeres dues lletres
-		for (int i=0; i<=1; i++){
-			char caracter = lletresInicial.charAt(i);
-			int ascii = (int) caracter;
-			if (ascii>=65 && ascii<=90){
-				boolInici=true;
-			}
+			String lletresInicial = matricula.substring(0,2);
+			String nums = matricula.substring(2,5);
+			String lletresFinal = matricula.substring(5,7);
 			
-			if (caracter == 'I'
-			|| caracter == 'O'
-			|| caracter == 'Q'
-			|| caracter == 'U'){
-				boolInici=false;
-			}
+			boolean boolInici = false;
+			boolean boolNums = false;
+			boolean boolFinal = false;
 			
-		}
-		
-		
-		if (boolInici==true){
-			// Validació nombres
-			for (int i=0; i<=2; i++){
-				char caracter = nums.charAt(i);
+			// Validació primeres dues lletres
+			for (int i=0; i<=1; i++){
+				char caracter = lletresInicial.charAt(i);
+				int ascii = (int) caracter;
+				if (ascii>=65 && ascii<=90){
+					boolInici=true;
+				}
 				
-				if (Character.isDigit(caracter)){
-					boolNums=true;
+				if (caracter == 'I'
+				|| caracter == 'O'
+				|| caracter == 'Q'
+				|| caracter == 'U'){
+					boolInici=false;
 				}
-				else {
-					boolNums=false;
-				}
+				
 			}
 			
 			
-			if (boolNums==true) {
-		
-				// Validació dues lletres finals
-				for (int i=0; i<=1; i++){
-					char caracter = lletresFinal.charAt(i);
-					int ascii = (int) caracter;
+			if (boolInici==true){
+				// Validació nombres
+				for (int i=0; i<=2; i++){
+					char caracter = nums.charAt(i);
 					
-					if (ascii>=65 || ascii<=90){
-						boolFinal=true;
+					if (Character.isDigit(caracter)){
+						boolNums=true;
 					}
-					
-					if (caracter == 'I'
-					|| caracter == 'O'
-					|| caracter == 'Q'
-					|| caracter == 'U'){
-						boolFinal=false;
+					else {
+						boolNums=false;
 					}
-					
-					
 				}
+				
+				
+				if (boolNums==true) {
+			
+					// Validació dues lletres finals
+					for (int i=0; i<=1; i++){
+						char caracter = lletresFinal.charAt(i);
+						int ascii = (int) caracter;
+						
+						if (ascii>=65 || ascii<=90){
+							boolFinal=true;
+						}
+						
+						if (caracter == 'I'
+						|| caracter == 'O'
+						|| caracter == 'Q'
+						|| caracter == 'U'){
+							boolFinal=false;
+						}
+						
+						
+					}
+				}
+				
 			}
 			
+			
+			if (boolFinal == true) {
+				System.out.println("És una matrícula italiana vàlida");
+			}
+			
+			else {
+				System.out.println("No és una matrícula italiana vàlida");
+			}
 		}
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		if (boolFinal == true) {
-			System.out.println("És una matrícula italiana vàlida");
-		}
-		
-		else {
-			System.out.println("No és una matrícula italiana vàlida");
-		}
 
 	}
 }
