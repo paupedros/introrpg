d87b98b5d33de5b5fc84f69e42d759c6abc109b5
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-07 09:44:25 +0100

    prgtest 31_68

---
+++ 31_68_segona_igual_penultima/SegonaIgualPenultima.java
@@ -0,0 +1,36 @@
+
+/* En aquest programa demanarem textos i direm si la segona lletra es igual a la penultima */
+
+public class SegonaIgualPenultima {
+	public static void main(String[] args) {
+		
+		System.out.println("Ves introduïnt texts (finalitza amb enter sol)");
+		String text = Entrada.readLine();
+		
+		while (!text.isBlank()){
+			
+			if (text.length() <= 1){
+				System.out.println("Segona diferent a penúltima");
+			}
+			
+			
+			else {
+				char segona = text.charAt(1);
+				char penultima = text.charAt(text.length()-2);
+				
+				
+				if (segona == penultima){
+					System.out.println("Segona igual a penúltima");
+				}
+				else {
+					System.out.println("Segona diferent a penúltima");
+				}
+			}
+			
+			text = Entrada.readLine();
+		}
+		System.out.println("Adéu");
+		
+	}
+}
+
