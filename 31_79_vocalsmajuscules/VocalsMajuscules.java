0a08d0cca781a83b4974330e54587a7e3983e364
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-14 08:55:21 +0100

    prgtest 31_79

---
+++ 31_79_vocalsmajuscules/VocalsMajuscules.java
@@ -0,0 +1,32 @@
+
+/* En aquest programa introduirem un text i convertirem les vocals en majuscules */
+
+public class VocalsMajuscules{
+	public static void main(String[] args){
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		text = text.toLowerCase();
+		String vocals = "aeiou";
+		
+		for (int i=0; i<text.length(); i++){
+			char ch = text.charAt(i);
+			// Comprovar si el caracter es una vocal
+			boolean vocal = false;
+			for (int n=0; n<vocals.length(); n++){
+				if (ch == vocals.charAt(n)){
+					vocal = true;
+					ch = Character.toUpperCase(ch);
+					break;
+				}
+			}
+			
+			System.out.print(ch);
+			
+			
+		}
+		System.out.println();
+		
+	}
+}
+
+
