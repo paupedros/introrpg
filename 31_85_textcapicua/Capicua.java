47704a4b95e33381f4152837206b5866590c341f
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-15 10:12:58 +0100

    prgtest31_85

---
+++ 31_85_textcapicua/Capicua.java
@@ -0,0 +1,26 @@
+
+/* En aquest programa demanem un text i indiquem si es capicua o no */
+
+public class Capicua{
+	public static void main(String[] args){
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		String textNou = "";
+		
+		for (int i=text.length()-1; i>=0; i--){
+			char ch = text.charAt(i);
+			textNou+=ch;
+		}
+		
+		if(text.equals(textNou)){
+			System.out.println("És capicua");
+		}
+		else {
+			System.out.println("No és capicua");
+		}
+		
+	}
+}
+
+
+
