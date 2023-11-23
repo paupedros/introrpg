539fa838ac9524ee02e26a6d32766b4077ec1c83
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-23 13:28:11 +0100

    prgtest 32_05

---
+++ 32_05_quadrat_parametritzat/Quadrat.java
@@ -0,0 +1,34 @@
+
+/* En aquest programa dibuixarem quadrats amb X, amb parametritzacio */
+
+public class Quadrat{
+	public static void main(String[] args){
+		int num = Integer.parseInt(args[0]);
+		dibuixaQuadrat(num);
+		
+	}
+	
+	
+	public static void dibuixaQuadrat(int num){
+		// dibuixa un quadrat
+		for (int linia=1; linia <= num; linia++) {
+			// dibuixa una línia
+			dibuixaLinia(num);
+			// dibuixa un salt de línia
+	        System.out.println();
+       	}
+		
+	}
+	
+	public static void dibuixaLinia(int num){
+		for (int columna=1; columna <= num; columna++) {
+			// dibuixa un element de la línia
+	    	System.out.print(" X");
+		}
+	}
+}
+
+
+
+
+
