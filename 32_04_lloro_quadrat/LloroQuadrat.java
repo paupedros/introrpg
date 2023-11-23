6f91fd114fd95b1b00b39da0518b2c6e49e340a0
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-23 12:45:49 +0100

    prgtest 32_04

---
+++ 32_04_lloro_quadrat/LloroQuadrat.java
@@ -0,0 +1,70 @@
+
+/* En aquest programa farem un lloro que repeteix texts fins que troba un text buit, si el text és
+	'dibuixa quadrat', dibuixarà un quadrat, si és 'dibuixa rectangle', un rectangle */
+
+public class LloroQuadrat{
+	
+	public static void main(String[] args){
+		
+		System.out.println("El lloro espera paraula:");
+		String text = Entrada.readLine();
+		
+		while(!text.isBlank()){
+			if(text.equals("dibuixa quadrat")){
+				dibuixaQuadrat();
+				System.out.println("El lloro espera paraula:");
+				text = Entrada.readLine();
+				
+			}
+			else if(text.equals("dibuixa rectangle")){
+				dibuixaRectangle();
+				System.out.println("El lloro espera paraula:");
+				text = Entrada.readLine();
+			}
+			else{
+				System.out.println("El lloro repeteix: " + text);
+				System.out.println("El lloro espera paraula:");
+				text = Entrada.readLine();
+			}
+		}
+		
+		System.out.println("Adéu");
+		
+	}
+	
+	
+	
+	public static void dibuixaRectangle(){
+		// dibuixa un rectangle
+		for (int linia=1; linia <= 5; linia++) {
+			// dibuixa una línia
+			dibuixaLinia();
+			dibuixaLinia();
+			// dibuixa un salt de línia
+			System.out.println();
+       	}
+       	
+	}
+	
+	public static void dibuixaQuadrat(){
+		// dibuixa un quadrat
+		for (int linia=1; linia <= 5; linia++) {
+			// dibuixa una línia
+			dibuixaLinia();
+			// dibuixa un salt de línia
+			System.out.println();
+       	}
+		
+	}
+	
+	public static void dibuixaLinia(){
+		for (int columna=1; columna <= 5; columna++) {
+			// dibuixa un element de la línia
+	    	System.out.print(" X");
+		}
+		
+	}
+}
+
+
+
