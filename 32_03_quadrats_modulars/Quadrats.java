fe230fede0a1b9cac20d8d6282b61f00ddd583b7
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-23 11:56:10 +0100

    prgtest 31_03

---
+++ 32_03_quadrats_modulars/Quadrats.java
@@ -0,0 +1,44 @@
+
+/* En aquest programa demanem un nombre i dibuixarem quadrats de 5x5 amb X, utilitzant moduls */
+
+public class Quadrats{
+	public static void main(String[] args){
+		
+		
+		
+		dibuixaQuadrats();
+		
+	}
+	
+	public static void dibuixaQuadrats(){
+		System.out.println("Quants?");
+		int num = Integer.parseInt(Entrada.readLine());
+		for (int i=num; i>0; i--){
+			dibuixaQuadrat();
+			System.out.println();
+		}
+	}
+	
+	public static void dibuixaQuadrat(){
+		// dibuixa un quadrat
+		for (int linia=1; linia <= 5; linia++) {
+			// dibuixa una línia
+			dibuixaLinia();
+			// dibuixa un salt de línia
+	        System.out.println();
+       	}
+		
+	}
+	
+	public static void dibuixaLinia(){
+		for (int columna=1; columna <= 5; columna++) {
+			// dibuixa un element de la línia
+	    	System.out.print(" X");
+		}
+	}
+}
+
+
+
+
+
