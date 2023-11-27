e77e8279c3f4d1ee1f38e8abbbcd70e6cbad1fef
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-27 20:48:27 +0100

    prgtest 32_09

--- 32_09_cadena_continua/CadenaContinua.java
+++ 32_09_cadena_continua/CadenaContinua.java
@@ -1,43 +1,22 @@
 
 /* En aquest programa demanem un text i un enter, allargarem la cadena fins a que tingui la llargada
-	del enter donat, a partir de la cadena */
+	del enter donat, a partir de la cadena. Tot el proces d'allargar la cadena es fara
+	mitjançant un modul parametritzat */
 
 public class CadenaContinua {
+	// Demanem les dades d'entrada
 	public static void main(String[] args){
 		System.out.println("Text?");
 		String text = Entrada.readLine();
 		
-		
-		
-		
 		if (!text.isBlank()){
 			System.out.println("Nombre?");
-			int num = Integer.parseInt(Entrada.readLine());
-			String nova = "";
-			if (num<1){
-				
+			int longitud = Integer.parseInt(Entrada.readLine());
+			
+			if (longitud<1){
 			}
 			else {
-				int i = 0;
-				while(nova.length()<=num-1){
-					if (i>=text.length()){
-						i=0;
-					}
-					nova = nova.concat("" + text.charAt(i));
-					i++;
-				}
-			
-			
-			
-			/*	for (int i=0; nova.length()<=num-1; i++){ 
-				// Mentre que la llargada de la nova cadena sigui més petita o igual al nombre indicat -1
-					if (i>=text.length()){
-						i=0;
-					}
-					nova = nova.concat("" + text.charAt(i));
-					
-				}*/
-				System.out.println(nova);
+				mostraCadenaContinua(text, longitud);
 			}
 			
 		}
@@ -46,6 +25,21 @@ public class CadenaContinua {
 		}
 		
 		
+		
+	}
+	
+	public static void mostraCadenaContinua(String text, int longitud){
+		String nova = "";
+		int i = 0;
+		while(nova.length()<=longitud-1){
+			if (i>=text.length()){
+				i=0;
+			}
+			nova = nova.concat("" + text.charAt(i));
+			i++;
+		}
+				
+		System.out.println(nova);
 		
 	}
 }
