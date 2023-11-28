9e7ec2a114bdda3206553c18f46019d9c5725b87
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-28 08:28:21 +0100

    prgtest 32_13

---
+++ 32_13_nomes_lletres/NomesLletres.java
@@ -0,0 +1,38 @@
+
+/* En aquest programa introduim un text i el retornem amb les lletres separades per comes */
+
+public class NomesLletres{
+	public static void main(String[] args){
+		
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		
+		filtraLletres(text);
+		
+		
+		
+	}
+	
+	public static void filtraLletres(String text){
+		String textNou = "";
+		boolean primer = false;
+		for (int i=0; i<text.length(); i++){
+			char caracter = text.charAt(i);
+			
+			if (Character.isAlphabetic(caracter)){
+				if (primer==false){
+					textNou = textNou.concat("" + caracter);
+					primer=true;
+				}
+				else {
+					textNou = textNou.concat(", " + caracter);
+				}
+				
+			}
+		}
+		
+		System.out.println(textNou);
+	}
+	
+}
+
