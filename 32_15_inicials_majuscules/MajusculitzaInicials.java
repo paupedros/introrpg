9c35a484e15d3e993b45fc438773949392ad6b61
paupedros <2023_pau.pedros@iticbcn.cat>
2023-11-28 08:36:28 +0100

    prgtest 32_15

---
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -0,0 +1,50 @@
+
+/* En aquest programa introduim un text i capitalitzarem cada paraula que estigui entre caracters que
+	no siguin lletres. Les dades d'entrada les obtenim amb el main, el tractament d'aquestes dades
+	amb un modul parametritzat */
+
+public class MajusculitzaInicials{
+	public static void main(String[] args){
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		text = text.toUpperCase();
+		
+		majusculitzaInicials(text);
+		
+	}
+	
+	public static void majusculitzaInicials(String text){
+		String textNou = "";
+		boolean paraula = false;
+		
+		// Recorrem el text
+		for (int i=0; i<text.length(); i++){
+			char ch = text.charAt(i);
+			if(!paraula){ // Si no estem en una paraula
+				if(Character.isLetter(ch)){ // Si el caracter es una lletra
+					ch = Character.toUpperCase(ch);
+					paraula = true; // Estem en una paraula
+				}
+			}
+			else { // Si estem en una paraula
+				if(Character.isLetter(ch)){ // Si el caracter es una lletra
+					ch = Character.toLowerCase(ch);
+				}
+			}
+			if(!Character.isLetter(ch)) { // Si no és una lletra
+				paraula = false;
+			}
+			
+			
+			
+			textNou+= ch;
+			
+		}
+		System.out.println(textNou);
+	}
+	
+}
+
+
+
+
