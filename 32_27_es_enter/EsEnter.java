801fd2a977b334e6e4d68fc8ce63234aac5c7a43
paupedros <2023_pau.pedros@iticbcn.cat>
2023-12-12 10:30:06 +0100

    prgtest 32_27

---
+++ 32_27_es_enter/EsEnter.java
@@ -0,0 +1,26 @@
+
+/* En aquest programa introduirem un text i ens dira si es un enter o no, 
+	parara quan es trobi amb el text buit */
+
+public class EsEnter{
+	public static void main(String[] args){
+		System.out.println("Introdueix texts (enter sol per finalitzar)");
+		String text = Entrada.readLine();
+		
+		while(!text.isBlank()){
+			text = text.strip();
+			boolean number = UtilString.esEnter(text);
+			if (!number){
+				System.out.println("No és enter");
+			}
+			else {
+				System.out.println("És enter");
+			}
+			text = Entrada.readLine();
+			
+		}
+		System.out.println("Adéu");
+	}
+}
+
+
