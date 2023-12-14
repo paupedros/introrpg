bead03d5f0338b1356744cdeabf9cf7fa9aa1067
paupedros <2023_pau.pedros@iticbcn.cat>
2023-12-14 12:58:48 +0100

    prgtest 32_29

---
+++ 32_29_cadena_continua/CadenaContinua.java
@@ -0,0 +1,33 @@
+
+/* En aquest programa demanem un text i un enter, allargarem la cadena fins a que tingui la llargada
+	del enter donat, a partir de la cadena 
+	Implementacio del modul cadenaContinua per a fer-ho externament, i la funcio esEnter per comprovar 
+	si el nombre indtroduit es un enter.
+*/
+
+public class CadenaContinua {
+	public static void main(String[] args){
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		
+		if (!text.isBlank()){
+			System.out.println("Nombre?");
+			String num = Entrada.readLine();
+			if(UtilString.esEnter(num)){
+				int numInt = UtilString.aEnter(num, true);
+				if (numInt>1){
+					System.out.println(UtilString.cadenaContinua(text, numInt));
+				}
+			}
+			return;
+		}
+		
+		System.out.println("error");
+		
+		
+		
+		
+	}
+}
+
+
