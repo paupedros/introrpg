1e5ab9e798f9025d7f956f387ea8200c66f2f93b
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-23 18:31:05 +0100

    prgtest 32_35

---
+++ 32_35_nota_mes_alta_v2/NotaMesAlta.java
@@ -0,0 +1,56 @@
+/**
+ * En aquest programa recollim notes d'un alumne i al final en diem quina ha sigut la nota mes alta.
+ * a de mes direm quines han sigut totes les notes
+ * 
+ */
+public class NotaMesAlta {
+    public static void main(String[] args) {
+        System.out.println("Introdueix les notes (-1 per finalitzar)");
+        int nota = Integer.parseInt(Entrada.readLine());
+        String notes = "";
+        while (nota != -1) {
+            notes += nota;
+            nota = Integer.parseInt(Entrada.readLine());
+        }
+
+        if(notes.length() <= 1){
+            System.out.println("Com a mínim calen dues notes");
+            return;
+        }
+
+        int notaMesAlta = getNotaMesAlta(notes);
+        notes = transformNotes(notes);
+        
+        System.out.println("La nota més alta és " + notaMesAlta + "de les introduïdes: " + notes);
+        
+    }
+
+    public static int getNotaMesAlta (String notes){
+        int notaMesAlta = 0;
+        for (int i = 0; i < notes.length(); i++) {
+            char ch = notes.charAt(i);
+            if(Character.getNumericValue(ch) >= notaMesAlta){
+                notaMesAlta = Character.getNumericValue(ch);
+            }
+        }
+        return notaMesAlta;
+    }
+
+    public static String transformNotes(String text){
+        String newText = "";
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			if (i == 0) {
+				newText += ch;
+				continue;
+			}
+            else if(i == text.length() - 1){
+                newText += " i " + ch;
+                continue;
+            }
+			newText += ", " + ch;
+		}
+		return newText;
+    }
+    
+}
