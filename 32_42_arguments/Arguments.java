9d05330072f18c4c3984bbb44d53ddeaf3bf26bf
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-23 22:57:55 +0100

    prgtest 32_42

---
+++ 32_42_arguments/Arguments.java
@@ -0,0 +1,25 @@
+/*
+ * En aquest programa identificarem si els arguments del main son o no enters
+ */
+
+public class Arguments {
+    public static void main(String[] args) {
+        
+        if(args.length == 0){
+            System.out.println("Cap argument");
+            return;
+        }
+    
+        for (int i = 0; i < args.length; i++) {
+            System.out.print("[" + i + "]" + " "); // La posicio de l'argument
+            String text = "";
+            if(UtilString.esEnter(args[i])){ // text de si es enter o no
+                text = "és enter";
+            }
+            else {
+                text = "no és enter";
+            }
+            System.out.println("\""+ args[i] + "\": " + text);
+        }
+    }
+}
