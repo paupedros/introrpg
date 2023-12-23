c7a2084732a6dab952d387c2bbf7419595c36a87
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-23 17:56:45 +0100

    prgtest 32_34

---
+++ 32_34_nota_mes_alta_v1/NotaMesAlta.java
@@ -0,0 +1,31 @@
+/**
+ * En aquest programa recollim notes d'un alumne i al final en diem quina ha sigut la nota mes alta
+ */
+public class NotaMesAlta {
+    public static void main(String[] args) {
+        System.out.println("Introdueix les notes (-1 per finalitzar)");
+        int nota = Integer.parseInt(Entrada.readLine());
+        String notes = "";
+        while (nota != -1) {
+            notes += nota;
+            nota = Integer.parseInt(Entrada.readLine());
+        }
+
+        if(notes.length() <= 1){
+            System.out.println("Com a mínim calen dues notes");
+            return;
+        }
+
+        int notaMesAlta = 0;
+        for (int i = 0; i < notes.length(); i++) {
+            char ch = notes.charAt(i);
+            System.out.println(ch);
+            if(Character.getNumericValue(ch) >= notaMesAlta){
+                notaMesAlta = Character.getNumericValue(ch);
+            }
+        }
+        System.out.println("La nota més alta és " + notaMesAlta);
+        
+    }
+    
+}
