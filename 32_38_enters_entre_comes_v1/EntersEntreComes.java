4fc9408d626d1e79a1749b8b0b06709c11775018
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-23 21:36:09 +0100

    prgtest 32_38

---
+++ 32_38_enters_entre_comes_v1/EntersEntreComes.java
@@ -0,0 +1,29 @@
+/*
+ * Mostrem separat en comes tres enters que estan dins d'un array
+ */
+
+public class EntersEntreComes {
+    public static void main(String[] args) {
+
+        int[] numeros;
+
+        numeros = new int[3];
+
+        numeros[0] = 1;
+
+        numeros[1] = 2;
+
+        numeros[2] = 3;
+
+        System.out.print(numeros[0]);
+
+        for (int i = 1; i < numeros.length; i++) {
+
+            System.out.print(", " + numeros[i]);
+
+        }
+
+        System.out.println();
+
+    }
+}
