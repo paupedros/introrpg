4a2d005092ef8268fe13e730769cc05783dc5615
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-26 01:31:19 +0100

    prgtest

--- 32_43_nomes_enters/SumaEnters.java
+++ 32_43_nomes_enters/SumaEnters.java
@@ -3,5 +3,11 @@
  */
 
 public class SumaEnters {
-    a
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 0; i < args.length; i++) {
+            suma += Integer.parseInt(args[i]);
+        }
+        System.out.println(suma);
+    }
 }
