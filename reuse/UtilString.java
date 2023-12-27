0c076f83fba910d05c9bdb59600a8a91c4924984
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-27 02:40:34 +0100

    prgtest 32_44

--- reuse/UtilString.java
+++ reuse/UtilString.java
@@ -459,6 +459,7 @@ public class UtilString {
 	// blanc, incloent els caracters en blanc quan inclouBlancs és cert
 
 	public static String[] separa(String text, boolean inclouBlancs) {
+		if(text.isEmpty()) return new String[0];
 		int numParaules = comptaParaules(text, inclouBlancs);
 		String[] newText = new String[numParaules];
 		int i = 0;
