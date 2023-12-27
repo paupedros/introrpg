ddc7c9197d65c47734694ca293e8832c288e0dbd
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2023-12-27 01:21:27 +0100

    prgtest 32_44

--- reuse/UtilString.java
+++ reuse/UtilString.java
@@ -477,8 +477,10 @@ public class UtilString {
 					i++;
 					continue;
 				}
+
+				// Si no es un espai
 				paraula += ch;
-				if (i + 1 < text.length() && Character.isWhitespace(text.charAt(i + 1))) {
+				if (i + 1 < text.length()-1 && Character.isWhitespace(text.charAt(i + 1))) {
 					// Si el caracter seguent no esta fora de rang i es un espai sortim
 					break; 
 				}
