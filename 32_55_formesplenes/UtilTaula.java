5abe9ca0e7812172be6c274497c3ec72a93c8c81
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-01-11 12:52:37 +0100

    prgtest

--- 32_55_formesplenes/UtilTaula.java
+++ 32_55_formesplenes/UtilTaula.java
@@ -240,8 +240,11 @@ public class UtilTaula {
         for (int i = 0; i < taula.length; i++) { // recorrem la taula filaxfila
             // recorrem els elements de la fila fins que estiguem al
             // element amb posicio coincident al valor de i
-            for (int j = taula[i].length; j>i; j--){
-                taula[i][j] = true;
+            for (int j = 0; j<=i; j++){
+                if(j >= i){
+                    taula[i][j] = true;
+
+                }
             }
         }
     }
