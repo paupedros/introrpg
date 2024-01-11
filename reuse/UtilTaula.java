d4bffd3bcf7705186fc53023af66d884934919e5
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-01-11 12:54:38 +0100

    prgtest

--- reuse/UtilTaula.java
+++ reuse/UtilTaula.java
@@ -240,10 +240,9 @@ public class UtilTaula {
         for (int i = 0; i < taula.length; i++) { // recorrem la taula filaxfila
             // recorrem els elements de la fila fins que estiguem al
             // element amb posicio coincident al valor de i
-            for (int j = 0; j<=i; j++){
-                if(j > i){
+            for (int j = 0; j<taula[i].length; j++){
+                if(j >= i){
                     taula[i][j] = true;
-
                 }
             }
         }
