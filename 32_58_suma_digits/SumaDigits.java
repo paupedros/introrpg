7a8f5000b24d117372767f3155ba1f2f3328d53d
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-01-16 09:16:09 +0100

    prgtest

---
+++ 32_58_suma_digits/SumaDigits.java
@@ -0,0 +1,30 @@
+/*
+ * Demanem un text, i anem sumant els digits recursivament
+ */
+public class SumaDigits {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        int nombres = sumaDigits(text);
+        System.out.println(nombres);
+    }
+    // Anem sumant els digits recursivament
+    public static int sumaDigits(String text) {
+        // cas base
+        if (text.isEmpty()) return 0;
+        int num = 0;
+
+        // tracta pas actual
+        char ch = text.charAt(0);
+        if (Character.isDigit(ch)) {
+            num = Integer.parseInt(Character.toString(ch));
+        }
+        // tracta pas recursiu
+        String rest = text.substring(1);
+        int restaText = sumaDigits(rest);
+
+        // composa resultat
+        int result = restaText + num;
+        return result;
+    }
+}
