202c3061e630a2fa59b03308a975027f5c851f8e
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-01-17 10:43:47 +0100

    prgtest

--- 32_55_formesplenes/UtilTaula.java
+++ 32_55_formesplenes/UtilTaula.java
@@ -509,7 +509,7 @@ public class UtilTaula {
         // Implementa la lògica addicional per a "*x*"
         int[] center = getCenter(taula); // [vertical, hori]
         inicialitzaCreuNPle(taula);
-        for (int i = taula.length - 1; i > 0; i--) {
+        for (int i = taula.length - 1; i >= 0; i--) {
             if (i > center[0]) { // si ja hem arribat al centre horitzontal
                 for (int j = taula[i].length - i; j < taula[i].length - (taula[i].length - i); j++) {
                     taula[i][j] = true;
