8573a904951533909b2eac1f46f26ee5e4602628
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-01-30 11:50:41 +0100

    prgtest 33_04

--- 33_04_lloro_memoria/Recordat.java
+++ 33_04_lloro_memoria/Recordat.java
@@ -11,7 +11,7 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 
 public class Recordat {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         String path = "records.txt";
         processaEntrada(path);
         System.out.println("D'acord");
@@ -19,11 +19,13 @@ public class Recordat {
         System.out.println("Adéu");
 
     }
+
     /*
      * Demana frases per entrada estàndard i les guarda a un fitxer
+     *
      * @param nomFitxer: nom del fitxer
      */
-    public static void processaEntrada(final String nomFitxer) throws IOException{
+    public static void processaEntrada(final String nomFitxer) throws IOException {
         FileWriter fileWriter = new FileWriter(nomFitxer);
         BufferedWriter sortida = new BufferedWriter(fileWriter);
 
@@ -42,17 +44,22 @@ public class Recordat {
 
     /*
      * Mostra el contingut del fitxer amb nom nomFitxer
+     *
      * @param nomFitxer: el nom del fitxer del que es mostrarà el contingut
      */
-     public static void mostraRecords(final String nomFitxer) throws IOException {
+    public static void mostraRecords(final String nomFitxer) throws IOException {
         FileReader fileReader = new FileReader(nomFitxer);
         BufferedReader input = new BufferedReader(fileReader);
 
         int i = 0;
-        while(true){
+        while (true) {
             String linia = input.readLine();
-            if (linia == null && i==0) System.out.println("El lloro no recorda res");
-            if(linia == null) break;
+            if (linia == null && i == 0) {
+                System.out.println("El lloro no recorda res");
+                i++;
+            }
+            if (linia == null)
+                break;
             System.out.println("El lloro recorda: " + linia);
         }
 
