8246591321e99abc731aaf62a2212e8994252707
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-07 09:18:26 +0100

    prgtest 33_06

---
+++ 33_08_alumnes/CercaAlumnes.java
@@ -0,0 +1,107 @@
+/*
+ * Programa que llegeix dades d'un csv amb dades dels alumnes
+ * i les seves notes
+ */
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class CercaAlumnes {
+
+    static class Alumne {
+        String nom;
+        String email;
+        int edat;
+        boolean esOient;
+        int[] notes;
+    }
+
+    public static Alumne construeixAlumne(String nom, String email,
+            int edat, boolean esOient,
+            int[] notes) {
+        Alumne alumne = new Alumne();
+        alumne.nom = nom;
+        alumne.email = email;
+        alumne.edat = edat;
+        alumne.esOient = esOient;
+        alumne.notes = notes;
+        return alumne;
+    }
+
+    public static void mostraAlumne(Alumne alumne) {
+        System.out.println("Alumne: " + alumne.nom);
+        System.out.println("- email: " + alumne.email);
+        System.out.println("- edat: " + alumne.edat);
+        System.out.println("- és oïent: " + (alumne.esOient ? "Sí" : "No"));
+        System.out.println("- notes: " + notesACsv(alumne.notes));
+    }
+
+    public static String alumneAString(Alumne alumne) {
+        return String.format(
+                "Alumne(nom: \"%s\", email: \"%s\", " +
+                        "edat: %d, esOient: %b, notes: {%s})",
+                alumne.nom, alumne.email, alumne.edat, alumne.esOient,
+                notesACsv(alumne.notes));
+    }
+
+    // converteix un array de notes a CSV
+    // Té en comptes els valors NP com a -1
+    public static String notesACsv(int[] notes) {
+        String notas = "";
+        for (int i = 0; i < notes.length; i++) {
+            if (i == notes.length - 1) {
+                notas += normaNote(notes[i]);
+                continue;
+            }
+            notas += normaNote(notes[i]) + ",";
+        }
+        return notas;
+    }
+
+    // Normalitza les notes, si hi ha una nota -1 la canviarem a NP
+    public static String normaNote(int nota) {
+        if (nota == -1)
+            return "NP";
+        return Integer.toString(nota);
+    }
+
+    public static String alumneACsv(Alumne alumne) {
+        return String.format("%s,%s,%f,%b,%s", alumne.nom, alumne.email, alumne.edat, alumne.esOient,
+                notesACsv(alumne.notes));
+
+    }
+
+    public static Alumne csvAAlumne(String csv) {
+        Alumne alumne = new Alumne();
+
+        String[] csvArray = csv.split(",");
+
+        
+
+        return alumne;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // assegura que hi ha el criteri de cerca
+        // XXX a completar
+
+        // declaracions, inicialitzacions, apertura de fitxer, ignora línia de
+        // capçaleres, etc.
+        // XXX a completar
+
+        while (true) {
+            // llegeix entrada i finalitza bucle si no en queden més
+            // XXX a completar
+
+            // converteix l'entrada a Alumne
+            // XXX a completar
+
+            // comprova si el criteri de cerca es troba dins del nom o
+            // el email. Si és així, mostra'l
+            // XXX a completar
+        }
+        // consideracions finals com ara el tancament del fitxer
+        // XXX a completar
+    }
+}
