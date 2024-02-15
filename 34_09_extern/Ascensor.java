0343394cdc41fcc62edcb748f0ffb5cdf4f9aa8b
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-15 13:22:09 +0100

    prgtest

---
+++ 34_09_extern/Ascensor.java
@@ -0,0 +1,8 @@
+/*
+ * Declaracio de la classe Ascensor i les seves propietats per a ser utilitzada
+ * en UsaAscensor
+ */
+
+public class Ascensor {
+    int pis = -1;
+}
---
+++ 34_09_extern/UsaAscensor.java
@@ -0,0 +1,9 @@
+/*
+ * Programa que utilitza la classe Ascensor
+ */
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("L'ascensor creat des de fora està al pis " + ascensor.pis);
+    }
+}
