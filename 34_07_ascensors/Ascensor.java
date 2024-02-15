4c645ac68643105a8da754c852b93d094ad10a1c
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-15 13:10:52 +0100

    prgtest

--- 34_07_ascensors/Ascensor.java
+++ 34_07_ascensors/Ascensor.java
@@ -13,7 +13,7 @@ public class Ascensor {
     }
 
     public static void main(String[] args){
-        if (args.length != 1) {
+        if (args.length != 1 || Integer.parseInt(args[0]) == 0) {
             System.out.println("Cap ascensor");
             return;
         }
