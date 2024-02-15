bac6f5b02bfe7d1a4ddf15366424f7b416216d57
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-15 08:55:43 +0100

    helped alex

--- proves/alex/ClassificaMatricules.java
+++ proves/alex/ClassificaMatricules.java
@@ -13,50 +13,65 @@ public class ClassificaMatricules {
     private static String rutaInvalides = "desconegudes.txt";
 
     public static void main(String[] args) throws IOException {
+        // Obrir el fitxer 'italianes.txt' en escriptura
+        BufferedWriter sortidaValides = new BufferedWriter(new FileWriter(rutaValides));
+        // Obrir el fitxer 'desconegudes.txt' en escriptura
+        BufferedWriter sortidaInvalides = new BufferedWriter(new FileWriter(rutaInvalides));
+        sortidaInvalides.close();
+        sortidaValides.close();
         separadorMatricules();
     }
 
     public static void separadorMatricules() throws IOException {
-        //Obrir el fitxer 'llegides' en lectura
+        // Obrir el fitxer 'llegides' en lectura
         BufferedReader input = new BufferedReader(new FileReader(rutaLectura));
 
-        //Obrir el fitxer 'italianes.txt' en escriptura
-        BufferedWriter sortidaValides = new BufferedWriter(new FileWriter(rutaValides));
-        //Obrir el fitxer 'desconegudes.txt' en escriptura
-        BufferedWriter sortidaInvalides = new BufferedWriter(new FileWriter(rutaInvalides));
 
-        String linia = input.readLine();    //agafa la primera linia del document
-        while (linia != null) {
-            linia = linia.trim();
-            if (matriculaItalianaValida(linia) && !esMatriculaRepetida(linia, rutaValides)) {
-                sortidaValides.write(linia);
-                sortidaValides.newLine();
-            }
+
+
+        while (true) {
+            String linia = input.readLine(); // agafa la primera linia del document
+            if (linia == null)
+                break;
             if (linia.isEmpty()) {
-                linia = input.readLine();
                 continue;
             }
-            if (!matriculaItalianaValida(linia) && !esMatriculaRepetida(linia, rutaInvalides)) {
-                sortidaInvalides.write(linia);
-                sortidaInvalides.newLine();
+            linia = linia.strip();
+            System.out.println(esMatriculaRepetida(linia, rutaValides));
+
+            if (matriculaItalianaValida(linia)) {
+                if (!esMatriculaRepetida(linia, rutaValides)) {
+                    // Obrir el fitxer 'italianes.txt' en escriptura
+                    BufferedWriter sortidaValides = new BufferedWriter(new FileWriter(rutaValides, true));
+                    sortidaValides.write(linia);
+                    sortidaValides.newLine();
+                    sortidaValides.close();
+                    continue;
+                }
+            } else {
+                if (!esMatriculaRepetida(linia, rutaInvalides)) {
+                    // Obrir el fitxer 'desconegudes.txt' en escriptura
+                    BufferedWriter sortidaInvalides = new BufferedWriter(new FileWriter(rutaInvalides, true));
+                    sortidaInvalides.write(linia);
+                    sortidaInvalides.newLine();
+                    sortidaInvalides.close();
+                    continue;
+                }
             }
-            linia = input.readLine();
         }
-        //Tanquem els filers jeje
+        // Tanquem els filers jeje
         input.close();
-        sortidaValides.close();
-        sortidaInvalides.close();
     }
 
     public static boolean esMatriculaRepetida(String matricula, String rutaFitxer) throws IOException {
         // Obrir en lectura el fitxer desitjat
         BufferedReader input = new BufferedReader(new FileReader(rutaFitxer));
-        matricula = matricula.strip();
-
         while (true) {
             String linia = input.readLine();
-            if (linia == null) break;
-            linia = linia.strip();
+            System.out.println(matricula + " " + linia);
+            if (linia == null)
+                break;
+            linia = linia.trim();
             if (linia.equals(matricula)) {
                 input.close();
                 return true;
@@ -68,36 +83,36 @@ public class ClassificaMatricules {
 
     public static boolean matriculaItalianaValida(String matricula) {
 
-        if (matricula.length()==7 &&
-        esLletraValidaPerMatriculaItaliana(matricula.charAt(0)) &&
-        esLletraValidaPerMatriculaItaliana(matricula.charAt(1)) &&
-        esLletraValidaPerMatriculaItaliana(matricula.charAt(5)) &&
-        esLletraValidaPerMatriculaItaliana(matricula.charAt(6)) &&
-        Character.isDigit(matricula.charAt(2)) &&
-        Character.isDigit(matricula.charAt(3)) &&
-        Character.isDigit(matricula.charAt(4))   ) {
+        if (matricula.length() == 7 &&
+                esLletraValidaPerMatriculaItaliana(matricula.charAt(0)) &&
+                esLletraValidaPerMatriculaItaliana(matricula.charAt(1)) &&
+                esLletraValidaPerMatriculaItaliana(matricula.charAt(5)) &&
+                esLletraValidaPerMatriculaItaliana(matricula.charAt(6)) &&
+                Character.isDigit(matricula.charAt(2)) &&
+                Character.isDigit(matricula.charAt(3)) &&
+                Character.isDigit(matricula.charAt(4))) {
             return true;
         }
         return false;
     }
+
     public static boolean esLletraValidaPerMatriculaItaliana(char ch) {
 
         boolean esLletraValida = false;
-        //Si la lletra es alguna d'aquestas, tampoc val'
+        // Si la lletra es alguna d'aquestas, tampoc val'
         String filtre = "IOUQ";
         boolean passaFiltre = true;
-        for (int j=0; j<filtre.length(); j++) {
-            if (ch == filtre.charAt(j) ) {
-                passaFiltre=false;
+        for (int j = 0; j < filtre.length(); j++) {
+            if (ch == filtre.charAt(j)) {
+                passaFiltre = false;
                 break;
             }
         }
 
-        if (ch>='A' && ch<='Z' && passaFiltre) {
+        if (ch >= 'A' && ch <= 'Z' && passaFiltre) {
             esLletraValida = true;
         }
         return esLletraValida;
     }
 
- }
-
+}
