57308b79f945d7b5fa37db16766fde45b73271c9
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-19 11:45:31 +0100

    prgtest

---
+++ 34_11_moviment/Ascensor.java
@@ -0,0 +1,11 @@
+/*
+ * Declaracio de la classe Ascensor i les seves propietats per a ser utilitzada
+ * en UsaAscensor
+ * Afegim el moviment
+ */
+
+public class Ascensor {
+    public int pis = -1;
+    public String moviment = "aturat";
+}
+
---
+++ 34_11_moviment/UsaAscensor.java
@@ -0,0 +1,13 @@
+/*
+ * Programa que utilitza la classe Ascensor i ens indica en quin
+ * pis esta l'ascensor creat des de fora
+ */
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("Pis inicial: " + ascensor.pis);
+        System.out.println("Moviment inicial: " + ascensor.moviment);
+        ascensor.moviment = "pujant";
+        System.out.println("Mobiment final: " + ascensor.moviment);
+    }
+}
