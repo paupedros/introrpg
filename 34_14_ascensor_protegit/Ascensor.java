65683ea46e984253d4e10f7372d16cdf571fac1e
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-20 09:29:44 +0100

    prgtest

---
+++ 34_14_ascensor_protegit/Ascensor.java
@@ -0,0 +1,30 @@
+/*
+ * Declaracio de la classe Ascensor i les seves propietats per a ser utilitzada
+ * en UsaAscensor, les propietats son privades
+ * Afegim el moviment
+ *
+ * Creem els setters i getters
+ */
+
+public class Ascensor {
+    private int pis = -1;
+    private String moviment = "aturat";
+
+    public int getPis() {
+        return pis;
+    }
+
+    public String getMoviment() {
+        return moviment;
+    }
+
+    public void setPis(int newPis) {
+        if(newPis >= -1 && newPis <= 10)
+            pis = newPis;
+    }
+    public void setMoviment(String newMoviment) {
+        String moviments = "aturat,pujant,baixant";
+        if (moviments.contains(newMoviment))
+            moviment = newMoviment;
+    }
+}
---
+++ 34_14_ascensor_protegit/UsaAscensor.java
@@ -0,0 +1,29 @@
+/*
+ * Programa que utilitza la classe Ascensor amb els getters i setters
+ */
+
+public class UsaAscensor {
+
+    /* XXX */
+
+    public static void main(String[] args) {
+
+        int pisFinal = Integer.parseInt(args[0]);
+        String movimentFinal = args[1];
+
+        Ascensor ascensor = new Ascensor();
+
+        System.out.println("Pis inicial: " + ascensor.getPis());
+
+        System.out.println("Moviment inicial: " + ascensor.getMoviment());
+
+        ascensor.setPis(pisFinal);
+        ascensor.setMoviment(movimentFinal);
+
+        System.out.println("Pis final: " + ascensor.getPis());
+
+        System.out.println("Moviment final: " + ascensor.getMoviment());
+
+    }
+
+}
