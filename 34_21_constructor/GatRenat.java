e1bcf65d58ce6f29e61e86d51314ae458c2d783d
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-22 14:05:20 +0100

    prgtest

--- 34_21_constructor/GatRenat.java
+++ 34_21_constructor/GatRenat.java
@@ -12,6 +12,8 @@ public class GatRenat {
         return String.format("Vides: %d. Posició: %s", vides, posicio);
     }
     public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+
     public static void main(String[] args) {
         GatRenat renat = new GatRenat();
         System.out.println(renat);
