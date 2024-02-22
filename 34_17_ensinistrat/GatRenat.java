a537988328fd21a174032a7d764bd6df5a7a795d
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-22 10:01:54 +0100

    prgtest

---
+++ 34_17_ensinistrat/GatRenat.java
@@ -0,0 +1,73 @@
+/*
+ * Declaracio de la classe GatRenat amb propietats privades,
+ * i amb setters i getters per poder treballar amb les propietats.
+ * Afegim els metodes per saber la seva posicio i si es viu.
+ * Metodes per a ensinistrar al Gat Renat: aixecat, seu i estirat
+ */
+
+public class GatRenat {
+    private int vides = 7;
+    private String posicio = "estirat";
+
+    public int getVides() {
+        return vides;
+    }
+
+    public void setVides(int vida) {
+        if (vida >= 0)
+            vides = vida;
+    }
+
+    public String getPosicio() {
+        return posicio;
+    }
+
+    public void setPosicio(String pos) {
+
+        if (pos.equals("dret")
+                || pos.equals("estirat")
+                || pos.equals("assegut"))
+            posicio = pos;
+    }
+
+    public boolean esViu() {
+        return vides > 0;
+    }
+
+    public boolean esDret() {
+        return posicio.equals("dret");
+    }
+
+    public boolean esAssegut() {
+        return posicio.equals("assegut");
+    }
+
+    public boolean esEstirat() {
+        return posicio.equals("estirat");
+    }
+
+    public String aixecat(){
+        if (!posicio.equals("dret")){
+            setPosicio("dret");
+            return "ja m'aixeco";
+        }
+        return "passo de fer res";
+    }
+
+    public String seu(){
+        if (!posicio.equals("assegut")){
+            setPosicio("assegut");
+            return "ja m'estiro";
+        }
+        return "passo de fer res";
+    }
+
+    public String estirat(){
+        if (!posicio.equals("estirat")){
+            setPosicio("estirat");
+            return "ja m'estiro";
+        }
+        return "passo de fer res";
+    }
+
+}
---
+++ 34_17_ensinistrat/UsaGatRenat.java
@@ -0,0 +1,14 @@
+/*
+ * Programa per utilitzar la classe GatRenat amb els metodes per
+ * poder ensinistrar-lo i que ens fagi cas.
+ */
+
+public class UsaGatRenat {
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();
+        System.out.println("El Renat diu: "+ renat.aixecat());
+        System.out.println("El Renat diu: "+ renat.seu());
+        System.out.println("El Renat diu: "+ renat.estirat());
+        System.out.println("El Renat diu: "+ renat.estirat());
+    }
+}
