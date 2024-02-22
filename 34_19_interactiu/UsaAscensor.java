9d26116cf50d8ad9e1b3a563c4d60306a9fea3b0
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-22 11:59:06 +0100

    prgtest

---
+++ 34_19_interactiu/UsaAscensor.java
@@ -0,0 +1,40 @@
+/*
+ * Programa on utilitzarem l'ascensor.
+ * Li indicarem per entrada estandard al pis que on estem, com que comença
+ * amb el moviment pujant doncs pujara fins a dalt de tot i
+ * baixara fins la planta baixa
+ */
+
+public class UsaAscensor {
+    public static int llegeixEnter() {
+        String resposta = Entrada.readLine();
+        if (! UtilString.esEnter(resposta)) {
+            return -2;
+        }
+        return Integer.parseInt(resposta);
+    }
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("Pis inicial");
+        ascensor.setPis(llegeixEnter());
+        System.out.printf("Inicialment l'ascensor està %s%n", ascensor.getPis());
+
+        // aturat a pujant
+        boolean resposta = ascensor.arrencaAmunt();
+        System.out.printf("En arrencar amunt l'ascensor respon %b i està %s%n",
+                resposta, ascensor.getMoviment());
+        // demanem que baixi sense aturar
+        resposta = ascensor.arrencaAbaix();
+        System.out.printf("En arrencar a baix l'ascensor respon %b i està %s%n",
+                resposta, ascensor.getMoviment());
+
+        // passem del pis indicat al la planta baixa
+        System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
+        while (! ascensor.esAbaix()) {
+            ascensor.seguentPis();       // passa al següent pis
+            System.out.println("\t ara està "+ ascensor.getPis());
+        }
+        ascensor.aturat();
+        System.out.printf("Està %s. Final de trajecte!%n", ascensor.comEsta());
+    }
+}
