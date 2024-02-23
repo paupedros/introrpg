4023900511f3866318e6bdee9529fbd216f99bf7
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-23 10:43:17 +0100

    prgtest 34_22
    Hora: 2024-02-23 10:43:17

---
+++ 34_22_constructor_parametritzat/GatRenat.java
@@ -0,0 +1,24 @@
+/*
+ * En aquest programa reutilitzem la classe GatRenat,
+ * pero en aquest cas utilitzem un constructor per
+ * asignar els valors per defecte de les propietats.
+ */
+
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    public GatRenat(int vides, String posicio) {
+        vides = 7;
+        posicio = "estirat";
+    }
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posició: %s", vides, posicio);
+    }
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "estirat"));
+    }
+}
