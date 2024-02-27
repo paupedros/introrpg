ff8793abd5fbab4abe5bc3608dd2b309d8e1c750
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-27 08:57:23 +0100

    prgtest 34_23
    Hora: 2024-02-27 08:57:23

---
+++ 34_23_constructor_posicio/GatRenat.java
@@ -0,0 +1,44 @@
+/*
+ * Nova versio que implementa per a la inicialitzacio de la posicio de GatRenat
+ * si el constructor rep una posicio no valida la posicio sera estirat
+ */
+
+public class GatRenat {
+    private int vides = 7;
+    private String posicio = "estirat";
+
+    public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+
+    public int getVides() {
+        return vides;
+    }
+
+    public void setVides(int novesVides) {
+        if (novesVides >= 0) {
+            vides = novesVides;
+        }
+    }
+
+    public String getPosicio(){
+        return posicio;
+    }
+
+    public void setPosicio(String pos) {
+        if (pos.equals("dret")
+                || pos.equals("estirat")
+                || pos.equals("assegut"))
+            posicio = pos;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vides: %d", vides);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "dret"));
+    }
+}
