8353b7113003127fd015c1f520a23b9516ddb808
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-27 10:36:46 +0100

    prgtest 34_26
    Hora: 2024-02-27 10:36:46

---
+++ 34_26_hora/Hora.java
@@ -0,0 +1,120 @@
+/**
+ * Hora
+ */
+public class Hora {
+    private int hores = 0;
+    private int minuts = 0;
+    private int segons = 0;
+
+    public Hora(){
+        this(0,0,0);
+    }
+
+    public Hora(int hores, int minuts, int segons){
+        if (hores < 0 || minuts < 0 || segons < 0){
+            hores = 0;
+            minuts = 0;
+            segons = 0;
+        }
+
+    }
+
+/* -------------------------------------------------------------------------- */
+/*                              GETTERS & SETTERS                             */
+/* -------------------------------------------------------------------------- */
+
+    public int getHores(){
+        return hores;
+    }
+
+    public int getMinuts(){
+        return minuts;
+    }
+
+    public int getSegons(){
+        return segons;
+    }
+
+    public void setHores(int hores) {
+        this.hores = hores;
+    }
+
+    public void setMinuts (int minuts) {
+        this.minuts = minuts;
+    }
+
+    public void setSegons(int segons) {
+        this.segons = segons;
+    }
+
+/* -------------------------------------------------------------------------- */
+/*                           INCREMENTA I DECREMENTA                          */
+/* -------------------------------------------------------------------------- */
+    public void incrementa(){
+        segons++;
+    }
+
+    public void incrementa(int segons) {
+        this.segons += segons;
+    }
+
+    public void decrementa() {
+        segons--;
+    }
+
+    public void decrementa(int segons) {
+        this.segons -= segons;
+    }
+
+    public int compareTo(Hora hora){
+        // Obtenim la hora en segons
+        int hora1Num = (this.getHores())*3600 + (this.getMinuts())*60 + this.getSegons();
+        int hora2Num = (hora.getHores())*3600 + (hora.getMinuts())*60 + hora.getSegons();
+        if (hora1Num < hora2Num) return -1;
+        if (hora2Num < hora1Num) return 1;
+        return 0;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%d %02d %02d", hores, minuts, segons);
+    }
+
+
+    /**
+     * Compara dues hores i retorna l'operador corresponent
+     * Per exemple, si hora1 és menor que hora2, l'operador serà "<". Els
+     * altres dos valors possibles són ">" i "=="
+     *
+     * @param hora1: primera hora a comparar
+     * @param hora2: segona hora a comparar
+     * @return operador resultant
+     */
+    private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
+        int comparacio = hora1.compareTo(hora2);
+        if (comparacio < 0) {
+            return "<";
+        } else if (comparacio > 0) {
+            return ">";
+        } else {
+            return "==";
+        }
+    }
+
+    public static void main(String[] args) {
+        Hora hora1 = new Hora();
+        Hora hora2 = new Hora(0, 0, 2);
+        System.out.printf("Inicialment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
+        hora1.incrementa();
+        hora2.decrementa();
+        System.out.printf("Finalment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+
+    }
+}
---
+++ 34_27_resum/resum.md
@@ -0,0 +1,37 @@
+# Què he aprés dels constructors?
+
+*Resum on indicarem el que hem apres sobre els constructors exercici a exercici.*
+
+## 34_21. Posicionant des del constructor
+
+En aquest exercici he aprés a aplicar un constructor a una classe,
+per donar el valor de les seves propietats. A més amb el Override hem vist
+que es poden utilitzar moduls predefinits a la nostra manera.
+
+## 34_22. Posicio parametritzada
+
+Hem vist com s'indiquen parametres als constructors i com s'utilitzen.
+Amb aquest constructor podem fins i tot mostrar la instancia sense
+haver d'assignar-la a una variable.
+
+## 34_23. Posició protegida en néixer
+
+He apres que es poden utilitzar els setters dins d'un constructor. En el cas del gat Renat
+ha de tenir les propietats inicialitzades per a que els setters puguin funcionar correctament.
+
+## 34_24. Aquesta posició
+La utilitzacio del this. He utilitzat el this per a fer referencia directa de la instancia
+a la que vull accedir per al setter. Aixi el constructor tambe accedeix directament a la
+instancia indicada per al this.
+
+## 34_25. Molts constructors
+
+Utilitzant tots els metodes que hem anat construint els fem servir per crear uns constructors
+que utilitzarant el this(), tots menys un que utilitza els setters. Aixo el que fa es que
+poguem fer servir el overload amb els constructors, ja que tots apunten al mes complet.
+
+## 34_26. La classe Hora
+
+
+
+
