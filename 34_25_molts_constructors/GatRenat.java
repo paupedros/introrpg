0f527b331239e23f0608619234f583727f08fbe1
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-02-27 09:22:26 +0100

    prgtest 34_25
    Hora: 2024-02-27 09:22:26

---
+++ 34_25_molts_constructors/GatRenat.java
@@ -0,0 +1,55 @@
+public class GatRenat {
+    private int vides;
+    private String posicio;
+
+    public GatRenat() {
+        this(7,"estirat");
+    }
+
+    public GatRenat(int vides) {
+        this(vides, "estirat");
+    }
+
+    public GatRenat(String posicio){
+        this(7,posicio);
+    }
+
+    public GatRenat(int vides, String posicio){
+        setVides(vides);
+        setPosicio(posicio);
+    }
+
+    public int getVides() {
+        return vides;
+    }
+
+    public void setVides(int novesVides) {
+        if (novesVides >= 0) {
+            vides = novesVides;
+        }
+    }
+
+    public String getPosicio() {
+        return posicio;
+    }
+
+    public void setPosicio(String pos) {
+        if (pos.equals("dret")
+                || pos.equals("estirat")
+                || pos.equals("assegut"))
+            this.posicio = pos;
+    }
+
+    public static void main(String[] args) {
+        GatRenat[] renats = {
+                new GatRenat(), // tot per defecte
+                new GatRenat(8), // 8 vides i posició per defecte
+                new GatRenat("dret"), // posició dret i vides per defecte
+                new GatRenat(8, "dret") // 8 vides i posició dret
+
+        };
+        for (GatRenat renat : renats) {
+            System.out.println(renat);
+        }
+    }
+}
