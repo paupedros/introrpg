d3da55ba103c26a6b730c5b638a675a8adbb096f
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-03-05 09:53:45 +0100

    prgtest 34_29
    Hora: 2024-03-05 09:53:45

--- 34_29_unic/UsaGatRenat.java
+++ 34_29_unic/UsaGatRenat.java
@@ -10,9 +10,9 @@ public class UsaGatRenat {
     private static void canviaPosicio(String novaPosicio) {
         // Tradueix novaPosicio: 1 -> estirat, 2 -> assegut, 3 -> dret,
         // altrament es queda com estava
-        if (novaPosicio.equals("1")) novaPosicio = "estirat";
-        else if (novaPosicio.equals("2")) novaPosicio = "assegut";
-        else if (novaPosicio.equals("3")) novaPosicio = "dret";
+        if (novaPosicio.equals("1") || novaPosicio.equals("estirat")) novaPosicio = "estirat";
+        else if (novaPosicio.equals("2") || novaPosicio.equals("assegut")) novaPosicio = "assegut";
+        else if (novaPosicio.equals("3") || novaPosicio.equals("dret")) novaPosicio = "dret";
         GatRenat.getInstancia(novaPosicio);
     }
 }
