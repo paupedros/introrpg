2dc42ac6b66bf84b88c637e416ee2387e53bd6d5
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-03-12 09:38:53 +0100

    prgtest 34_33
    Hora: 2024-03-12 09:38:53

---
+++ 34_33_renat_es_gat/UsaGatRenat.java
@@ -0,0 +1,13 @@
+/**
+ * UsaGatRenat; programa que utilitzarem per a provar l'herencia de classes
+ * entre Gat i GatRenat 
+ */
+public class UsaGatRenat {
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();
+        System.out.println("El Renat diu: "+ renat.aixecat());
+        System.out.println("El Renat diu: "+ renat.seu());
+        System.out.println("El Renat diu: "+ renat.estirat());
+        System.out.println("El Renat diu: "+ renat.estirat());
+    }
+}
