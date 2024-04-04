09265ae4d5127b3fe1c1a4d83bee3f532cf62bda
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-04 13:22:14 +0200

    prgtest 34_36
    Hora: 2024-04-04 13:22:14

--- 34_36_interfagat/Gat.java
+++ 34_36_interfagat/Gat.java
@@ -2,45 +2,48 @@
  * Classe de Gat, que determina les propietats i getters/setters dels gats
  */
 
-public class Gat implements EsserViu{
+public class Gat implements EsserViu {
     private int vides = 7;
     private String nom = "anònim";
 
-    Gat(String nom){
-        if (!nom.isBlank() || nom != null)
+    Gat(String nom) {
+        if (nom.isBlank() || nom.equals(null)) {
+            this.nom = "anònim";
+        } else
             this.nom = nom;
-        this.nom = "anònim";
+
     }
 
-    Gat(String nom, int vides){
+    Gat(String nom, int vides) {
         this.nom = nom;
         this.vides = vides;
     }
 
-    public String getNom(){
+    public String getNom() {
         return nom;
     }
 
-    public int getVides(){
+    public int getVides() {
         return vides;
     }
 
-    public int setVides(int vides){
-        if (vides >= 0){
+    public int setVides(int vides) {
+        if (vides >= 0) {
             this.vides = vides;
         }
         return this.vides;
     }
 
     @Override
-    public boolean esViu(){
-        if (vides > 0) return true;
+    public boolean esViu() {
+        if (vides > 0)
+            return true;
         return false;
     }
 
     @Override
-    public String mor(){
-        if(vides - 1 == 0){
+    public String mor() {
+        if (vides - 1 == 0) {
             vides--;
             return "ja l'he espifiada";
         }
@@ -49,12 +52,12 @@ public class Gat implements EsserViu{
     }
 
     @Override
-    public String reviu(){
-        if(esViu()){
+    public String reviu() {
+        if (esViu()) {
             vides++;
             return "encara miolo";
         }
         vides++;
         return "guai!";
-    }    
+    }
 }
