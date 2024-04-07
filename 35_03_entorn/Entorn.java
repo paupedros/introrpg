e1c8ff97696177096cad7b3f9b2c83ceadfba016
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-07 15:17:21 +0200

    prgtest 35_03
    Hora: 2024-04-07 15:17:21

--- 35_03_entorn/Entorn.java
+++ 35_03_entorn/Entorn.java
@@ -23,7 +23,6 @@ public class Entorn {
                 case "afegeix":
                     entorn.processaAfegeix();
                     break;
-/*
                 case "cerca":
                     entorn.processaCerca();
                     break;
@@ -33,7 +32,6 @@ public class Entorn {
                 case "elimina":
                     entorn.processaElimina();
                     break;
-*/
                 default:
                     mostraErrorComandaDesconeguda();
             }
@@ -42,49 +40,141 @@ public class Entorn {
     }
 
     /* ---- PROCESSAMENT ---- */
-    
-    public void processaAfegeix(){
+
+    public void processaAfegeix() {
         // ---- NOM ----
         System.out.print("nom (enter cancel·la)> ");
         String nom = Entrada.readLine();
-        if (nom.isBlank()) return;
+        if (nom.isBlank())
+            return;
 
         // ---- PREU ----
         System.out.print("preu (en cèntims)> ");
-        String preuString = Entrada.readLine();
-        int preu = tractaDades(preuString);
+        int preu = tractaDades(Entrada.readLine());
         // Si no es un enter sera -1
-        if (preu == -1) return;
+        if (preu == -1)
+            return;
 
         // ---- ESTOC ----
         System.out.print("estoc (enter sense estoc)> ");
-        String estocString = Entrada.readLine();
-        int estoc = tractaDades(estocString);
+        int estoc = tractaDades(Entrada.readLine());
         // Si no es un enter sera -1
-        if (estoc == -1) return;
+        if (estoc == -1)
+            return;
 
-        // Afegeix aqui ->
+        // Afegim el vi a la botiga
+        Vi newVi = botiga.afegeix(new Vi(nom, preu, estoc));
+        if (newVi == null) {
+            System.out.println("ERROR: no s'ha pogut afegir");
+            return;
+        }
+        System.out.println(String.format("Introduït:%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n", nom, preu, estoc));
     }
 
-    private int tractaDades(String data){
+    public void processaCerca(){
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+        Vi vi = botiga.cerca(nom);
+        // Si no es troba el vi
+        if(vi == null) {
+            System.out.println("No trobat");
+            return;
+        }
+        // Si es troba el vi
+        System.out.println(String.format("Trobat:%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n", nom, vi.getPreu(), vi.getEstoc()));
+    }
+
+    public void processaModifica(){
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+        Vi vi = botiga.cerca(nom);
+        // Si no es troba el vi
+        if(vi == null) {
+            System.out.println("No trobat");
+            return;
+        }
+        // Si es troba el vi
+
+        // ---- PREU ----
+        System.out.print(String.format("preu (enter %d)> ", vi.getPreu()));
+        String preuString = Entrada.readLine();
+        // Si el preu no esta en blanc...
+        if (!preuString.isBlank()) {
+            // Si no es un enter donem l'error
+            if (!UtilString.esEnter(preuString)) {
+                System.out.println("ERROR: el valor ha de ser un enter positiu");
+                return;
+            }
+            // Si es un enter modifiquem el preu del vi
+            vi.setPreu(Integer.parseInt(preuString));
+        }
+
+        // ---- ESTOC ----
+        System.out.print(String.format("estoc (enter %d)> ", vi.getEstoc()));
+        String estocString = Entrada.readLine();
+        // Si el preu no esta en blanc...
+        if (!estocString.isBlank()) {
+            // Si no es un enter donem l'error
+            if (!UtilString.esEnter(estocString)) {
+                System.out.println("ERROR: el valor ha de ser un enter positiu");
+                return;
+            }
+            // Si es un enter modifiquem el preu del vi
+            vi.setEstoc(Integer.parseInt(estocString));
+        }
+
+        System.out.println(String.format("Modificat:%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n", vi.getNom(), vi.getPreu(), vi.getEstoc()));
+
+    }
+
+    public void processaElimina(){
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+        Vi vi = botiga.cerca(nom);
+        // Si no es troba el vi
+        if(vi == null) {
+            System.out.println("No trobat");
+            return;
+        }
+        // Si es troba el vi
+        System.out.println(String.format("A eliminar:%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n", vi.getNom(), vi.getPreu(), vi.getEstoc()));
+
+        // Demanem confirmacio
+        System.out.print("Segur?> ");
+        boolean confirm = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
+        if (confirm) {
+            Vi deleted = botiga.elimina(vi.getNom());
+            // Si no s'ha pogut eliminar diem error
+            if (deleted == null) {
+                System.out.println("ERROR: no s'ha pogut eliminar");
+                return;
+            }
+            // Si si s'ha pogut ho diem
+            System.out.println("Eliminat");
+            return;
+        }
+        System.out.println("No eliminat");
+    }
+
+    private int tractaDades(String data) {
         // Si la dada esta buida ho interpretem com un 0
-        if (data.isBlank()) return 0;
+        if (data.isBlank())
+            return 0;
         // Si no es un enter indiquem l'error i sortim de la comanda
-        if (!UtilString.esEnter(data)){
+        if (!UtilString.esEnter(data)) {
             System.out.println("ERROR: cal un enter positiu");
             return -1;
         }
         // Passem el preu a enter
         return Integer.parseInt(data);
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
     /* ---- MISSATGES ---- */
 
     public static void mostraBenvinguda() {
@@ -96,14 +186,14 @@ public class Entorn {
     }
 
     public static void mostraAjuda() {
-        System.out.println(String.format("Comandes disponibles:%najuda%ncerca%nafegeix%nmodifica%nelimina%nsurt%n"));
+        System.out.println(String.format("Comandes disponibles:%najuda%ncerca%nafegeix%nmodifica%nelimina%nsurt"));
     }
 
-    public static void mostraErrorComandaDesconeguda(){
+    public static void mostraErrorComandaDesconeguda() {
         System.out.println("ERROR: comanda no reconeguda. Escriviu help per ajuda");
     }
 
-    public static void mostraComiat(){
-        System.out.println("adeu");
+    public static void mostraComiat() {
+        System.out.println("adéu");
     }
 }
