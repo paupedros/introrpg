a00d0458a79cab25d4707c05813fd0d903b30a53
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-11 19:51:42 +0200

    prgtest 35_04
    Hora: 2024-04-11 19:51:42

--- 35_04_csv/Vi.java
+++ 35_04_csv/Vi.java
@@ -74,7 +74,7 @@ public class Vi {
     }
 
     public static Vi deArrayString(String[] array) {
-        if (array.length > 3) return null;
+        if (array.length != 3) return null;
         Vi vi = new Vi(array[0], Integer.parseInt(array[1]), Integer.parseInt(array[2]));
         if(!vi.esValid()) return null;
         return vi;
