c6b0f54d6a31bd6e6488cd5f722735d13c63781c
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-12 09:02:20 +0200

    prgtest 35_05
    Hora: 2024-04-12 09:02:20

---
+++ 35_05_vi_ampliat/Botiga.java
@@ -0,0 +1,142 @@
+/**
+ * Classe Botiga que contindra els vins que hi hauran
+ * te metodes per afegir, eliminar i cercar vins
+ */
+
+public class Botiga {
+    private int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+    private int index = 0;
+
+    Botiga() {
+        vins = new Vi[DEFAULT_MAX_VINS];
+    }
+
+    Botiga(int maxVins) {
+        vins = new Vi[maxVins];
+    }
+
+    public Vi afegeix(Vi vi) {
+        if (vi.esValid()) {
+            // Si no es troba cap vi amb el mateix nom al de afegir
+            Vi cercaVi = cerca(vi.getRef());
+            //System.out.println(cercaVi);
+            if (cercaVi == null) {
+                // Busca un espai null a la llista
+                for (int i = 0; i < vins.length; i++) {
+                    if (vins[i] == null) {
+                        vins[i] = vi;
+                        return vi;
+                    }
+                }
+                return null;
+            } else
+                return null;
+        } else
+            return null;
+    }
+
+    public Vi elimina(String refVi) {
+        refVi = Vi.normalitzaString(refVi);
+        Vi vi = cerca(refVi);
+        if (vi != null) {
+            // Si te estoc...
+            if (vi.getEstoc() > 0) {
+                return null;
+            } else {
+                for (int i = 0; i < vins.length; i++) {
+                    Vi deletedVi = vins[i];
+                    if (refVi.equals(vins[i].getRef())) {
+                        vins[i] = null;
+                        return deletedVi;
+                    }
+                }
+            }
+        }
+        return vi;
+    }
+
+    public Vi cerca(String ref) {
+        ref = Vi.normalitzaString(ref);
+        for (int i = 0; i < vins.length; i++) { // Per cada vi de la llista de vins
+            Vi vi = vins[i];
+            if (vi != null && ref.equalsIgnoreCase(vi.getRef()))
+                return vi;
+        }
+        return null;
+    }
+
+    public Vi cerca(Vi plantilla) {
+
+        for (Vi vi : vins) {
+            // STRINGS
+            // -------------
+            // Si la referencia no esta buida...
+            if (plantilla.getRef() != null)
+                // Si les referencies no coincideixen...
+                if (!plantilla.getRef().equalsIgnoreCase(vi.getRef()))
+                    continue;
+            if (plantilla.getNom() != null)
+                if (!plantilla.getNom().equalsIgnoreCase(vi.getNom()))
+                    continue;
+            if (plantilla.getLloc() != null)
+                if (!plantilla.getLloc().equalsIgnoreCase(vi.getLloc()))
+                    continue;
+            if (plantilla.getOrigen() != null)
+                if (!plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen()))
+                    continue;
+            if (plantilla.getTipus() != null)
+                if (!plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))
+                    continue;
+            if (plantilla.getRef() != null)
+                if (!plantilla.getCollita().equalsIgnoreCase(vi.getCollita()))
+                    continue;
+
+            // ENTERS
+            // -----------
+
+            if (plantilla.getPreu() == -1){
+                return vi;
+            }
+            else if (vi.getPreu() <= plantilla.getPreu()) return vi;
+
+            if (plantilla.getEstoc() == -1) return vi;
+            else if (vi.getEstoc() > plantilla.getEstoc()) return vi;
+
+        }
+        return null;
+    }
+
+    public void iniciaRecorregut() {
+        index = 0;
+    }
+
+    public Vi getSeguent() {
+        // Mentre que no ens passem del rang i el vi actual sigui null
+        while (index < vins.length && vins[index] == null) {
+            index++;
+        }
+
+        // Si no ens hem passat del rang
+        if (index < vins.length) {
+            Vi vi = vins[index];
+            index++;
+            return vi;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (Vi vi : vins) {
+            if (vi == null) {
+                result += "null\n";
+                continue;
+            }
+            result += vi.getNom();
+            result += "\n";
+        }
+        return result;
+    }
+}
---
+++ 35_05_vi_ampliat/Vi.java
@@ -0,0 +1,166 @@
+/**
+ * Classe del Vi, contindra les diferents propietats, constructors
+ * i metodes
+ */
+public class Vi {
+    private String ref = null;
+    private String nom = null;
+    private int preu = -1;
+    private int estoc = -1;
+    private String lloc = null;
+    private String origen = null;
+    private String tipus = null;
+    private String collita = null;
+
+    public Vi(String ref, String nom,
+            int preu, int estoc,
+            String lloc, String origen,
+            String tipus, String collita) {
+        this.ref = normalitzaString(ref);
+        this.nom = normalitzaString(nom);
+        this.setPreu(preu);
+        this.setEstoc(estoc);
+        this.setLloc(lloc);
+        this.origen = normalitzaString(origen);
+        this.tipus = normalitzaString(tipus);
+        this.collita = normalitzaString(collita);
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPreu() {
+        return preu;
+    }
+
+    public void setPreu(int preu) {
+        if (preu >= 0)
+            this.preu = preu;
+    }
+
+    public int getEstoc() {
+        return estoc;
+    }
+
+    public void setEstoc(int estoc) {
+        if (estoc >= 0)
+            this.estoc = estoc;
+    }
+
+    public String getLloc() {
+        return lloc;
+    }
+
+    public void setLloc(String lloc) {
+        this.lloc = lloc;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public String getCollita() {
+        return collita;
+    }
+
+    public static String normalitzaString(String text) {
+        if (!text.isBlank()) {
+            return text.trim().replaceAll("\\s+", " ");
+        }
+        if (text == null || text.isEmpty())
+            return null;
+        return null;
+    }
+
+    public boolean esValid() {
+        if (ref == null || ref.isBlank())
+            return false;
+        else if (nom == null || nom.isBlank())
+            return false;
+        else if (preu < 0)
+            return false;
+        else if (estoc < 0)
+            return false;
+        else if (lloc == null || lloc.isBlank())
+            return false;
+        else if (origen == null || origen.isBlank())
+            return false;
+        else if (tipus == null || tipus.isBlank())
+            return false;
+        else if (collita == null || collita.isBlank())
+            return false;
+        else
+            return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%n    Ref: %s%n    Nom: %s%n    Preu: %d%n    Estoc: %d%n    Lloc: %s%n    D.O.: %s%n    Tipus: %s%n    Collita: %s", ref, nom, preu, estoc, lloc, origen, tipus, collita);
+    }
+
+    public String[] aArrayString() {
+        // Obtenim els strings del numeros
+        String preu = String.valueOf(this.preu);
+        String estoc = String.valueOf(this.estoc);
+        // Creem l'array amb les dades del vi
+        String[] array = { this.ref, this.nom, this.tipus, preu, this.collita, estoc, this.origen, this.lloc };
+        return array;
+    }
+
+    public static Vi deArrayString(String[] array) {
+        // Si l'array es valid per a ser un vi, creem una instancia i si es valida la
+        // retornem
+        if (arrayValid(array)) {
+            String ref = array[0];
+            String nom = array[1];
+            String tipus = array[2];
+            int preu = Integer.parseInt(array[3]);
+            String collita = array[4];
+            int estoc = Integer.parseInt(array[5]);
+            String origen = array[6];
+            String lloc = array[7];
+            Vi vi = new Vi(ref, nom, preu, estoc, lloc, origen, tipus, collita);
+            if (vi.esValid())
+                return vi;
+        }
+        // Si no es valid retornem null
+        return null;
+    }
+
+    public static boolean arrayValid(String[] array) {
+        // Condicions per saber si un array no es valid per construir un vi:
+        /**
+         * Llargada diferent a 8
+         * El preu i el estoc no son enters
+         * La instancia de vi no es valida
+         */
+        if (array.length != 8)
+            return false;
+        if (!UtilString.esEnter(array[3]) || !UtilString.esEnter(array[5]))
+            return false;
+        // Constructor: ref, nom, preu, estoc, lloc, origen, tipus, collita
+        // Array: ref nom tipus preu collita estoc origen lloc
+        String ref = array[0];
+        String nom = array[1];
+        String tipus = array[2];
+        int preu = Integer.parseInt(array[3]);
+        String collita = array[4];
+        int estoc = Integer.parseInt(array[5]);
+        String origen = array[6];
+        String lloc = array[7];
+        Vi vi = new Vi(ref, nom, preu, estoc, lloc, origen, tipus, collita);
+        if (!vi.esValid())
+            return false;
+        return true;
+    }
+
+}
