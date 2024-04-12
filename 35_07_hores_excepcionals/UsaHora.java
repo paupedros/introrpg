20a99add82bcff48b667a45f658ef1a4054d0e1c
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-12 19:34:59 +0200

    prgtest 35_08
    Hora: 2024-04-12 19:34:59

--- 35_07_hores_excepcionals/UsaHora.java
+++ 35_07_hores_excepcionals/UsaHora.java
@@ -3,15 +3,33 @@
  */
 
 public class UsaHora {
-        public static void main(String[] args) {
-            System.out.println("Intentarem crear una hora amb valors no vàlids");
+    private static void canviaHora(Hora hora, int hores, int minuts, int segons) {
 
-            try {
-                Hora hora = new Hora(-1, 1, 1);
-            } catch (IllegalArgumentException e) {
-
-                System.out.println("Argument il·legal!");
-            }
+        try {
+            hora.setHores(hores);
+            hora.setMinuts(minuts);
+            hora.setSegons(segons);
+        } catch (Exception e) {
+            throw e;
+            //System.out.println(e);
         }
-
     }
+
+    public static void main(String[] args) {
+        Hora hora = new Hora();
+        System.out.println("Inicialment " + hora);
+        System.out.println("Si intentem assignar a hores un -1");
+        canviaHora(hora, -1, hora.getMinuts(), hora.getSegons());
+        System.out.println("El valor resultant és " + hora);
+        System.out.println();
+        System.out.println("Si intentem assignar a minuts un -1");
+        canviaHora(hora, hora.getHores(), -1, hora.getSegons());
+        System.out.println("El valor resultant és " + hora);
+        System.out.println();
+
+        System.out.println("Si intentem assignar a segons un -1");
+        canviaHora(hora, hora.getHores(), hora.getMinuts(), -1);
+        System.out.println("El valor resultant és " + hora);
+        System.out.println();
+    }
+}
