1115caa1c3a58e5173f080f18907bab0f3cfe0a4
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-30 10:57:24 +0200

    prgtest 52_03
    Hora: 2024-04-30 10:57:24

---
+++ 52_03_coordenada_y/Punt.java
@@ -0,0 +1,9 @@
+/*
+ * Classe que defineix el comportament i propietats Punt, utilitzada
+ * per aprendre a fer proves unitaries amb JUnit
+ */
+
+public class Punt {
+    public int getX() { return 0; }
+    public int getY(){ return 0; }
+}
