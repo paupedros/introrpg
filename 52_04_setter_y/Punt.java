28107cbed76c79d2760817c8018c64c6ceaa7784
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-04-30 18:33:04 +0200

    update

--- 52_04_setter_y/Punt.java
+++
@@ -1,9 +0,0 @@
-/*
- * Classe que defineix el comportament i propietats Punt, utilitzada
- * per aprendre a fer proves unitaries amb JUnit
- */
-
-public class Punt {
-    public int getX() { return 0; }
-    public int getY(){ return 0; }
-}
