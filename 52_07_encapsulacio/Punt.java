997623392efa91f5a50ae4ca228c41833f9c200d
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-05-02 19:31:52 +0200

    prgtest 52_07
    Hora: 2024-05-02 19:31:52

--- 52_07_encapsulacio/Punt.java
+++ 52_07_encapsulacio/Punt.java
@@ -5,38 +5,36 @@
 
 public class Punt {
     private int[] coord = {0, 0};
-    private int x = coord[0];
-    private int y = coord[1];
 
     public Punt(){
-        x = 0;
-        y = 0;
+        coord[0] = 0;
+        coord[1] = 0;
     }
 
     public Punt(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.coord[0] = x;
+        this.coord[1] = y;
     }
 
-    public int getX() { return x; }
-    public int getY(){ return y; }
+    public int getX() { return coord[0]; }
+    public int getY(){ return coord[1]; }
 
     public void setX(int x) {
-        this.x = x;
+        this.coord[0] = x;
     }
     public void setY(int y) {
-        this.y = y;
+        this.coord[1] = y;
     }
 
-    public void suma(Punt p) { x+=p.x; y+=p.y; }
+    public void suma(Punt p) { coord[0]+=p.coord[0]; coord[1]+=p.coord[1]; }
 
     @Override
     public String toString() {
-        return String.format("Punt(%d, %d)", x, y);
+        return String.format("Punt(%d, %d)", coord[0], coord[1]);
     }
 
     public boolean equals(Punt p) {
         // Retorna true si les coordenades son iguals
-        return x == p.getX() && y == p.getY();
+        return coord[0] == p.getX() && coord[1] == p.getY();
     }
 }
