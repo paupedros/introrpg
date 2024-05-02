16e1889f35774e2d932653efe95b9bb791ac706b
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-05-02 11:55:47 +0200

    prgtest 52_06
    Hora: 2024-05-02 11:55:47

--- 52_06_segment/TestSegment.java
+++ 52_06_segment/TestSegment.java
@@ -16,41 +16,41 @@ public class TestSegment {
         new Segment(p1, p2);
     }
 
-    @Test
-    public void getP1() {
-        Punt p1 = new Punt();
-        Punt p2 = new Punt();
-        Segment s = new Segment(p1, p2);
-        assertEquals(p1, s.getP1());
-    }
+    // @Test
+    // public void getP1() {
+    //     Punt p1 = new Punt();
+    //     Punt p2 = new Punt();
+    //     Segment s = new Segment(p1, p2);
+    //     assertEquals(p1, s.getP1());
+    // }
 
-    @Test
-    public void setP1() {
-        Punt p1 = new Punt();
-        Punt p2 = new Punt();
-        Segment s = new Segment(p1, p2);
-        Punt p3 = new Punt(3, 1);
-        s.setP1(p3);
-        assertEquals(p3, s.getP1());
-    }
+    // @Test
+    // public void setP1() {
+    //     Punt p1 = new Punt();
+    //     Punt p2 = new Punt();
+    //     Segment s = new Segment(p1, p2);
+    //     Punt p3 = new Punt(3, 1);
+    //     s.setP1(p3);
+    //     assertEquals(p3, s.getP1());
+    // }
 
-    @Test
-    public void getP2() {
-        Punt p1 = new Punt();
-        Punt p2 = new Punt();
-        Segment s = new Segment(p1, p2);
-        assertEquals(p2, s.getP2());
-    }
+    // @Test
+    // public void getP2() {
+    //     Punt p1 = new Punt();
+    //     Punt p2 = new Punt();
+    //     Segment s = new Segment(p1, p2);
+    //     assertEquals(p2, s.getP2());
+    // }
 
-    @Test
-    public void setP2() {
-        Punt p1 = new Punt();
-        Punt p2 = new Punt();
-        Segment s = new Segment(p1, p2);
-        Punt p3 = new Punt(3, 1);
-        s.setP2(p3);
-        assertEquals(p3, s.getP2());
-    }
+    // @Test
+    // public void setP2() {
+    //     Punt p1 = new Punt();
+    //     Punt p2 = new Punt();
+    //     Segment s = new Segment(p1, p2);
+    //     Punt p3 = new Punt(3, 1);
+    //     s.setP2(p3);
+    //     assertEquals(p3, s.getP2());
+    // }
 
     @Test
     public void constructor() {
