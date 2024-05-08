179468731e482cb84eca269c8ebd61f3b8103e5b
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-05-08 11:52:34 +0200

    prgtest 52_14
    Hora: 2024-05-08 11:52:34

--- 52_14_mes_tests/Client.java
+++ 52_14_mes_tests/Client.java
@@ -55,7 +55,24 @@ public class Client {
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer : lloguers) {
-            double quantitat = quantitatPerLloguer(lloguer);
+            double quantitat = 0;
+            switch (lloguer.getVehicle().getCategoria()) {
+                case Vehicle.BASIC:
+                    quantitat += 3;
+                    if (lloguer.getDies() > 3) {
+                        quantitat += (lloguer.getDies() - 3) * 1.5;
+                    }
+                    break;
+                case Vehicle.GENERAL:
+                    quantitat += 4;
+                    if (lloguer.getDies() > 2) {
+                        quantitat += (lloguer.getDies() - 2) * 2.5;
+                    }
+                    break;
+                case Vehicle.LUXE:
+                    quantitat += lloguer.getDies() * 6;
+                    break;
+            }
             // afegeix lloguers freqüents
             bonificacions++;
             // afegeix bonificació per dos dies de lloguer de Luxe
@@ -75,27 +92,5 @@ public class Client {
         resultat += "Import a pagar: " + total + "€\n" +
                 "Punts guanyats: " + bonificacions + "\n";
         return resultat;
-    }
-
-    private double quantitatPerLloguer(Lloguer lloguer) {
-        double quantitat = 0;
-        switch (lloguer.getVehicle().getCategoria()) {
-            case Vehicle.BASIC:
-                quantitat += 3;
-                if (lloguer.getDies() > 3) {
-                    quantitat += (lloguer.getDies() - 3) * 1.5;
-                }
-                break;
-            case Vehicle.GENERAL:
-                quantitat += 4;
-                if (lloguer.getDies() > 2) {
-                    quantitat += (lloguer.getDies() - 2) * 2.5;
-                }
-                break;
-            case Vehicle.LUXE:
-                quantitat += lloguer.getDies() * 6;
-                break;
-        }
-        return quantitat;
     }
 }
