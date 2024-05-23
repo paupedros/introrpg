aeb00454c56bc0ec8c626b7d473cb16d9547bede
Pau Pedrós Ribas <2023_pau.pedros@iticbcn.cat>
2024-05-23 14:13:55 +0200

    prgtest 36_06
    Hora: 2024-05-23 14:13:55

---
+++ 36_06_insert/Animal.java
@@ -0,0 +1,36 @@
+/*
+ * Classe que defineix les propietats i els comportaments que tenen
+ * els animals dintre del nostre Zoo
+ */
+
+public class Animal {
+    private int id = -1;
+    private String nom;
+    private Categoria categoria;
+
+    Animal(String nom, Categoria categoria) {
+        if (nom == null || nom.isBlank()) throw new IllegalArgumentException("El nom no pot ser null ni blanc");
+        if (categoria == null) throw new IllegalArgumentException("La categoria no pot ser null");
+        this.nom = nom;
+        this.categoria = categoria;
+    }
+
+    Animal(int id, String nom, Categoria categoria) {
+        this(nom, categoria);
+        if (id < 0) {
+            throw new IllegalArgumentException("L'identificador ha de ser positiu");
+        }
+        this.id = id;
+    }
+
+    public int getId() {
+        if (!idIndefinit()) return id;
+        return -1;
+    }
+    public String getNom() {return nom;}
+    public Categoria getCategoria() {return categoria;}
+
+    public boolean idIndefinit() {
+        return id < 0 ? true : false;
+    }
+}
